4cbc9117a9435cb76a84f6c600762ea8c67b9b22
osodyj <69102470+osodyj@users.noreply.github.com>
2026-01-21 01:18:47 -0600

    Update streamlit_app.py
    add json

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"Customize Your Smoothie :cup_with_straw")
+st.title(f"Customize Your Smoothie :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input("Name On Smoothie:")
@@ -39,4 +39,4 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
